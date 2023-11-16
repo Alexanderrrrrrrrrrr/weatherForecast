ae161f6b1ad67171967a6c70cb5cd15236eddb77
Dmitrii Filippov <dimitriusanin@yandex.ru>
2023-11-16 17:05:21 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { API_KEY, API_URL } from './constants';
+
+
+const queryType = {
+  current: 'current.json',
+  forecast: 'forecast.json'
+}
+
+const cities = {
+  Alanya: {
+    name:'Alanya'
+  },
+  NewYork :{
+    name: 'New York'
+  },
+  Moscow :{
+    name: 'Moscow'
+  }
+}
 
 function App() {
+  const [query, setQuery] = useState(queryType.current)
+  const [city, setCity] = useState(cities.Alanya.name)
+  const [data, setData] = useState(null)
+  useEffect(()=>{
+    fetch(`${API_URL}/${query}?key=${API_KEY}&q=${city}`)
+      .then(res => res.json())
+      .then(res =>{
+        setData(res.current)
+      }).catch(err => {
+        setData(null)
+      })
+  },[city, query])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <button onClick={()=>setQuery(queryType.forecast)}>show forecast</button>
+    <ul>
+      {
+        Object.keys(cities).map(key => <li key={key}>
+            <button onClick={() =>setCity(cities[key].name)}>{cities[key].name}</button>
+          </li>)
+      }
+    </ul>
+    {data && 
+      <ul>
+        <li className={data.temp_c < 20 ? 'cold': 'hot'}>
+          температура С: <span>{data.temp_c}</span>
+        </li>
+        <li>
+          температура F: <span>{data.temp_f}</span>
+        </li>
+      </ul>
+      
+    }
+   </div>
   );
 }
 
---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const API_KEY = '06ce4ff62a104438bb6114142231611'
+export const API_URL = 'http://api.weatherapi.com/v1'
